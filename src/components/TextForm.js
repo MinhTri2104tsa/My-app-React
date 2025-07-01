98e5618168faf8bd5cdd9300e4437a167f7756be
Trí Nguyễn Minh <mtri2105@gamil.com>
2025-07-01 21:46:43 +0700

    Dark Mode Text

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,8 +7,15 @@ export default function TextForm(props) {
     const handleLoClick = () => {
         setText(text.toLowerCase());
     }
+    const handleOnCopy = () => {
+        navigator.clipboard.writeText(text);
+    }
     const handleOnChange = (event) => {
         setText(event.target.value);
+    }
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
     }
     const [text, setText] = useState('Enter text here');
     return (
@@ -21,6 +28,8 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" type="submit" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-1" type="submit" onClick={handleLoClick}>Convert to Lowercase</button>
         <button className="btn btn-primary mx-1" type="submit" onClick={() => setText('')}>Clear Text</button>
+        <button className="btn btn-primary mx-1" type="submit" onClick={handleOnCopy}>Copy Text</button>
+        <button className="btn btn-primary mx-1" type="submit" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
     <div className='container my-3'>
         <h2>Your text summary</h2>
